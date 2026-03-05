69955df4af237f42e77c04cb2802c2b5156ac0b8
thinkorsmile <72007697+thinkorsmile@users.noreply.github.com>
2026-03-05 18:49:30 +0800

    修改

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 
-# SmartDNS
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 
 # argon theme
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
