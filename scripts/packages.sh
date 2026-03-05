9f6c839bbc83e67a1a90aa213b854c6c8c7bb0cb
thinkorsmile <72007697+thinkorsmile@users.noreply.github.com>
2026-03-05 14:55:30 +0800

    push origin

---
+++ scripts/packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# OpenClash
+git clone https://github.com/vernesong/OpenClash package/openclash
+
+# SmartDNS
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+
+# argon theme
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
